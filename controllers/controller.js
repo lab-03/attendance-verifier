79110403c0591507cec4d57b0e02d4ea2b6376bc
IIZODIACII <amr_x14@outlook.com>
2020-05-20 17:17:11 +0200

    Added qrCode invalidate end point

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,30 +5,12 @@ const fs = require("fs");
 const path = require("path");
 
 class Controller {
-  async getAllQrCodes(req, res) {
-    await request.get("http://localhost:5000/api/qrcodes/", function (
-      err,
-      response,
-      body
-    ) {
-      if (response.statusCode === 200) {
-        util.setSuccess(
-          response.statusCode,
-          JSON.parse(body).message,
-          JSON.parse(body).data
-        );
-      } else if (err || response.statusCode !== 200) {
-        util.setError(response.statusCode, JSON.parse(body).message);
-      }
-      return util.send(res);
-    });
-  }
   async getQrCode(req, res) {
     const { body } = req;
     await request.post(
       "http://localhost:5000/api/qrcodes/create",
       { form: body },
-      function (err, response, body) {
+      function(err, response, body) {
         if (response.statusCode === 200) {
           util.setSuccess(
             response.statusCode,
@@ -42,12 +24,27 @@ class Controller {
       }
     );
   }
-  async attend(req, res) {
+  async invalidate(req, res) {
+    const { body } = req;
+    await request.post(
+      "http://localhost:5000/api/qrcodes/end",
+      { form: body },
+      function(err, response, body) {
+        if (response.statusCode === 200) {
+          util.setSuccess(response.statusCode, JSON.parse(body).message);
+        } else if (err || response.statusCode !== 200) {
+          util.setError(response.statusCode, JSON.parse(body).message);
+        }
+        return util.send(res);
+      }
+    );
+  }
+  async attendByQr(req, res) {
     const { body } = req;
     await request.post(
       "http://localhost:5000/api/qrcodes/attend",
       { form: body },
-      function (err, response, body) {
+      function(err, response, body) {
         if (response.statusCode === 200) {
           util.setSuccess(response.statusCode, JSON.parse(body).message);
         } else if (err || response.statusCode !== 200) {
@@ -60,11 +57,11 @@ class Controller {
   async attendByFR(req, res) {
     var options = {
       method: "POST",
-      url: "https://fr-api.herokuapp.com//verify",
+      url: "https://fr-api.herokuapp.com/verify",
       headers: { "content-type": "application/json" },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(req.body)
     };
-    request(options, function (err, response) {
+    request(options, function(err, response) {
       const temp = JSON.parse(response.body).same_person;
       if (temp === true) {
         util.setSuccess(response.statusCode, temp);
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import Controller from "../controllers/controller";
 const routes = Router();
 
-routes.get("/qrcodes/", Controller.getAllQrCodes);
-routes.post("/qrcodes/attend", Controller.attend);
+routes.post("/qrcodes/attend", Controller.attendByQr);
 routes.post("/qrcodes/create", Controller.getQrCode);
-routes.post("/fr",Controller.attendByFR);
+routes.post("/qrcodes/end", Controller.invalidate);
+routes.post("/fr", Controller.attendByFR);
 
 export default routes;
